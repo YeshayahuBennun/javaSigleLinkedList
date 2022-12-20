75f6c5711d6b3f88fb1834c83ea8b2c5c401d6c7
YeshayahuBennun <ybennun@yahoo.com>
2022-12-20 08:56:47 +0200

    Java Single Linked List - #1

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        SinglyLinkedList sLL = new SinglyLinkedList();
+        sLL.createSinglyLinkedList(5);
+        System.out.println(sLL.head.value);
+    }
+}
