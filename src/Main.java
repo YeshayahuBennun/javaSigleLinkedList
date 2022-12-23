296739019345a78a61bcc9abeff9db9832503851
YeshayahuBennun <ybennun@yahoo.com>
2022-12-23 08:32:45 +0200

    Commit#5 - Searching in Singly Linked List

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,14 @@ public class Main {
         SinglyLinkedList sLL = new SinglyLinkedList();
 
         sLL.createSingleLinkedList(5);
-       // System.out.println(sLL.getHead().getValue());
+        // System.out.println(sLL.getHead().getValue());
 
-        sLL.insertLinkedList(6,1);
-        sLL.insertLinkedList(7,3);
-        sLL.insertLinkedList(8,4);
-        sLL.insertLinkedList(9,0);
+        sLL.insertLinkedList(6, 1);
+        sLL.insertLinkedList(7, 3);
+        sLL.insertLinkedList(8, 4);
+        sLL.insertLinkedList(9, 0);
         sLL.traverseSinglyLinkedList();
-     //   System.out.println(sLL.getHead().getNext().getNext().getValue());
-
+        sLL.searchNode(10);
 
 
     }
